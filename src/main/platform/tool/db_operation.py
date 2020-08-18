eb73c2e0bfadfc96e01956557c5a9d404201745d
yibo.feng <yibo.feng@thoughtworks.com>
2020-08-18 12:38:31 +0800

    update db_operation

--- src/main/platform/tool/db_operation.py
+++ src/main/platform/tool/db_operation.py
@@ -17,37 +17,42 @@ class DBOperation:
                                          password=self.mysql_password, charset='utf8')  # 连接数据库
             cursor = connection.cursor(cursor=pymysql.cursors.DictCursor)  # 得到一个可以执行SQL语句的光标对象，并且将结果作为字典返回
         except:
-            result_res = {"status": "执行失败，数据库配置错误", "result": ''}
-            return result_res
+            return {"status": "执行失败，数据库配置错误", "result": ''}
 
         # 当传入的参数个数大于sql中%s出现的次数n时,截取传入的参数，保留前n个参数
         if len(parm) >= sql.count('%s'):
-            parm = parm[:sql.count('%s')]
-            try:
-                result_num = cursor.execute(sql, parm)  # 执行语句
-                # result = cursor.fetchone()  # 获取首行数据
-                # result = cursor.fetchmany(2)  # 获取第几条数据
-                result = cursor.fetchall()  # 获取所有数据
+            sql_list = list(filter(None, sql.split(';')))  # 按照';'划分并取出""
+            count = 0
+            operation_list = []
+            for sql in sql_list:
+                location1 = count
+                count += sql.count('%s')
+                location2 = count
+                operation_list.append({"sql": sql, "parm": parm[location1: location2]})
 
-                connection.commit()  # 提交到数据库，不提交只是模拟的完成，并不会真正更新数据库
-                cursor.close()  # 关闭光标对象
-                connection.close()  # 关闭数据库连接
-            except:
-                result_res = {"status": "执行失败，sql语句错误", "result": ''}
-                return result_res
-            if result_num == 0:
-                result_res = {"status": "执行成功，无需操作或未查到任何结果", "result": ''}
+            # 执行sql语句，所有语句执行成功后才结束
+            result_num_list = []
+            result_list = []
+            for operation in operation_list:
+                sql, parm = operation['sql'], operation['parm']
+                try:
+                    result_num = cursor.execute(sql, parm)  # 执行语句
+                    # result = cursor.fetchone()  # 获取首行数据
+                    # result = cursor.fetchmany(2)  # 获取第几条数据
+                    result = cursor.fetchall()  # 获取所有数据
+                    # 收集执行结果
+                    result_num_list.append(result_num)
+                    if result != ():
+                        result_list.append(result)
+                except:
+                    return {"status": "执行失败，sql语句错误", "result": ''}
+            connection.commit()  # 提交到数据库，不提交只是模拟的完成，并不会真正更新数据库
+            cursor.close()  # 关闭光标对象
+            connection.close()  # 关闭数据库连接
+
+            if result_num_list.count(0) == len(result_num_list):
+                return {"status": "执行成功，无需操作或未查到任何结果", "result": ''}
             else:
-                result_res = {"status": "执行成功", "result": result}
-            return result_res
+                return {"status": "执行成功", "result": result_list}
         else:
-            result_res = {"status": "执行失败，参数缺失", "result": ''}
-            return result_res
-
-    # 先后执行多条语句
-    def db_operation_more(self, group):
-        count = []  # 创建一个数组
-        for i in group:
-            result_res = self.db_operation(i['sql'], i['parm'])
-            count.append(result_res)  # 将执行结果写入数组
-        return count
+            return {"status": "执行失败，参数缺失", "result": ''}
