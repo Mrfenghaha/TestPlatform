87b66d4843b33564b3ecf56a260b0e5730f4456f
yibofeng <951249103@qq.com>
2020-09-21 19:48:21 +0800

    restructure tool/mock_servers

--- src/main/platform/tool/mock_servers.py
+++ src/main/platform/tool/mock_servers.py
@@ -79,28 +79,32 @@ class MockReturn:
 
 class MockErrorHandle(MockReturn):
 
-    # 请求的错误处理-参数格式
-    def body_format_judge(self, request):
-        try:
-            json.dumps(request.json)
-        except ValueError:
-            log.info('mock请求body参数格式错误,没有按照json格式传参')
-            return self.error_return('')
-        else:
+    # 对于mock配置的url验证
+    def url_judge(self, parm_url):
+        url_list = []  # 查询数据库mock配置中存在的所有url并写入url_list=["","",""] url不能出现/
+        for url in database_func('mock_servers', 'get', 'all_url'):
+            url_list.append(url[0])
+        if parm_url in url_list:  # 如果请求的url存在于url_list即为该mock存在，否则按照404响应返回
             return True
+        else:
+            log.info('MockServer请求url不存在,响应404')
+            abort(404)  # url不存在
 
-    # 请求的错误处理-必填参数缺少(参数配置不为空时)  body_parm=["","",""]
-    def body_lack_judge(self, request, body_parm):
-        # 如果请求参数缺少(参数列表缺少任意一个)，按错误处理
-        for p in body_parm:
-            if p not in request.json:
-                return self.error_return(p)
-            else:
-                if request.json.get(p) == "":  # 如果请求参数值为“”(布尔值、int类型为空时会按照不符合json格式处理掉)
-                    return self.error_return("param " + p + "is error")
-                else:
-                    pass
-        return True
+    # 对于mock配置的method验证
+    def method_judge(self, parm_method, methods):
+        if parm_method == methods:  # 如果请求方式与mock配置的请求方式一致即为请求成功，否则按照405响应返回
+            return True
+        else:
+            log.info('MockServer请求方法错误,响应405')
+            abort(405)  # 请求方式错误
+
+    # 对于mock配置的is_available验证
+    def is_available_judge(self, is_available):
+        if is_available == 1:
+            return True
+        else:
+            log.info('MockServer请求url未启用,响应404')
+            abort(404)  # 如果mock不启用，报404
 
     # 请求的错误处理-headers必填缺少(headers配置不为空时)  headers_parm=["","",""]
     def headers_lack_judge(self, request, headers_parm):
@@ -115,98 +119,75 @@ class MockErrorHandle(MockReturn):
                     pass
         return True
 
+    # 请求的错误处理-参数格式
+    def body_format_judge(self, request):
+        try:
+            json.dumps(request.json)
+        except ValueError:
+            log.info('mock请求body参数格式错误,没有按照json格式传参')
+            return self.error_return('')
+        else:
+            return True
+
+    # 请求的错误处理-body必填参数缺少(参数配置不为空时)  body_parm=["","",""]
+    def body_lack_judge(self, request, body_parm):
+        # 如果请求参数缺少(参数列表缺少任意一个)，按错误处理
+        for p in body_parm:
+            if p not in request.json:
+                return self.error_return(p)
+            else:
+                if request.json.get(p) == "":  # 如果请求参数值为“”(布尔值、int类型为空时会按照不符合json格式处理掉)
+                    return self.error_return("param " + p + "is error")
+                else:
+                    pass
+        return True
+
 
 class MockServer(MockErrorHandle):
     def __init__(self, url, request):
         self.request = request
         self.url = url
 
-    # 对于存在的mock接口根据配置进行响应返回
-    def mock(self, is_available, delay, response):
-        if is_available == 0:
-            log.info('MockServer请求url未启用,响应404')
-            abort(404)  # 如果mock不启用，报404
-        else:
-            time.sleep(delay / 1000)  # 延时响应/ms
-            return self.correct_return(response)
-
     # 构建mock_server服务
     def mock_server(self):
-        url_list = []  # 查询数据库mock配置中存在的所有url并写入url_list=["","",""] url不能出现/
-        for url in database_func('mock_servers', 'get', 'all_url'):
-            url_list.append(url[0])
-        if self.url in url_list:  # 如果请求的url存在于url_list即为该mock存在，否则按照404响应返回
-            # 根据url信息获取所有配置mock信息,再根据mock配置信息中的mock_id、resp_code获取具体的response,拼接为完整的mock_info信息
+        if self.url_judge(self.url) is True:
             mock = database_func('mock_servers', 'get', 'first_by_url', self.url)
-            response = database_func('mock_response', 'get', 'first_by_mockId_code', mock['id'], mock['resp_code'])
-            mock_info = {"url": mock['url'], "method": mock['method'], "is_available": mock['is_available'],
-                         "delay": mock['delay'],
-                         "response": {"resp_body": response['resp_body'], "resp_status": response['resp_status'],
-                                      "resp_headers": response['resp_headers']}}
-            if self.request.method == mock_info['method']:  # 如果请求方式与mock配置的请求方式一致即为请求成功，否则按照405响应返回
-                return self.mock(mock_info['is_available'], mock_info['delay'], (mock_info['response']))
-            else:
-                log.info('MockServer请求方法错误,响应405')
-                abort(405)  # 请求方式错误
-        else:
-            log.info('MockServer请求url不存在,响应404')
-            abort(404)  # url不存在
-
-
-# 验证请求参数(验证是否满足json-是否有参数未传-是否有必填项为空)
-class MockServerRequest(MockErrorHandle):
-    def __init__(self, url, request):
-        self.request = request
-        self.url = url
-
-    # 对于存在的mock接口根据配置进行响应返回
-    def mock_request(self, is_available, delay, headers_parm, body_parm, request, response):
-        if is_available == 0:
-            log.info('MockServer请求url未启用,响应404')
-            abort(404)  # 如果mock不启用，报404
-        else:
-            time.sleep(delay / 1000)  # 延时响应/ms
-            # 获取请求headers参数是否缺少(以及必填项是否填写)结果
-            headers_lack_judge = self.headers_lack_judge(request, headers_parm)
-            if headers_lack_judge is True:
-                # 再判断如果请求body为空时，请求body参数配置是否也为空（均为空按照参数校验通过处理）
-                if not request.json and body_parm == []:
-                    log.info('mock请求未设置任何参数,直接返回结果')
+            if self.method_judge(self.request.method, mock['methods']) is True:
+                if self.is_available_judge(mock['is_available']) is True:
+                    resp = database_func('mock_response', 'get', 'first_default_by_mockId', mock['id'])
+                    response = {"resp_body": resp['body'], "resp_status": resp['status'],
+                                "resp_headers": resp['headers']}
+                    time.sleep(mock['delay'] / 1000)  # 延时响应/ms
                     return self.correct_return(response)
-                else:
-                    # 再获取请求body格式是否错误(满足json格式)结果
-                    body_format_judge = self.body_format_judge(request)
-                    if body_format_judge is True:
-                        # 再获取请求body参数是否缺少(以及必填项是否填写)结果
-                        body_lack_judge = self.body_lack_judge(request, body_parm)
-                        if body_lack_judge is True:
+
+    # 构建mock_server服务，验证请求参数(验证是否满足json-是否有参数未传-是否有必填项为空)
+    def mock_server_request(self):
+        if self.url_judge(self.url) is True:
+            mock = database_func('mock_servers', 'get', 'first_by_url', self.url)
+            if self.method_judge(self.request.method, mock['methods']) is True:
+                if self.is_available_judge(mock['is_available']) is True:
+                    resp = database_func('mock_response', 'get', 'first_default_by_mockId', mock['id'])
+                    response = {"resp_body": resp['body'], "resp_status": resp['status'],
+                                "resp_headers": resp['headers']}
+
+                    headers_lack_judge = self.headers_lack_judge(self.request, list(response.headers_parm))
+                    if headers_lack_judge is True:
+                        # 再判断如果请求body为空时，请求body参数配置是否也为空（均为空按照参数校验通过处理）
+                        if not self.request.json and list(response.body_parm) == []:
+                            log.info('mock请求未设置任何参数,直接返回结果')
                             return self.correct_return(response)
                         else:
-                            return body_lack_judge
+                            # 再获取请求body格式是否错误(满足json格式)结果
+                            body_format_judge = self.body_format_judge(self.request)
+                            if body_format_judge is True:
+                                # 再获取请求body参数是否缺少(以及必填项是否填写)结果
+                                body_lack_judge = self.body_lack_judge(self.request, list(response.body_parm))
+                                if body_lack_judge is True:
+                                    time.sleep(mock['delay'] / 1000)  # 延时响应/ms
+                                    return self.correct_return(response)
+                                else:
+                                    return body_lack_judge
+                            else:
+                                return body_format_judge
                     else:
-                        return body_format_judge
-            else:
-                return headers_lack_judge
-
-    # 构建mock_server服务
-    def mock_server_request(self):
-        url_list = []  # 查询数据库mock配置中存在的所有url并写入url_list=["","",""] url不能出现/
-        for url in database_func('mock_servers', 'get', 'all_url'):
-            url_list.append(url[0])
-        if self.url in url_list:  # 如果请求的url存在于url_list即为该mock存在，否则按照404响应返回
-            # 根据url信息获取所有配置mock信息,再根据mock配置信息中的resp_code获取具体的response,拼接为完整的mock_info信息
-            mock = database_func('mock_servers', 'get', 'first_by_url', self.url)
-            response = database_func('mock_response', 'get', 'first_by_mockId_code', mock.id, mock.resp_code)
-            mock_info = {"url": mock.url, "method": mock.method, "is_available": mock.is_available, "delay": mock.delay,
-                         "headers_parm": list(response.headers_parm), "body_parm": list(response.body_parm),
-                         "response": {"resp_body": response.resp_body, "resp_status": response.resp_status,
-                                      "resp_headers": response.resp_headers}}
-            if self.request.method == mock_info['method']:  # 如果请求方式与mock配置的请求方式一致即为请求成功，否则按照405响应返回
-                return self.mock_request(mock_info['is_available'], mock_info['delay'], mock_info['headers_parm'],
-                                         mock_info['body_parm'], self.request, mock_info['response'])
-            else:
-                log.info('MockServer请求方法错误,响应405')
-                abort(405)  # 请求方式错误
-        else:
-            log.info('MockServer请求url不存在,响应404')
-            abort(404)  # url不存在
+                        return headers_lack_judge
